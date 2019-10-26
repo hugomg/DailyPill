12280539afe6429e5e328b44ac2a6f1817ea922e
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-10-26 16:25:09 -0300

    Reminder is visible above lock screen

--- app/src/main/java/com/example/thyroidhelper/ReminderActivity.kt
+++ app/src/main/java/com/example/thyroidhelper/ReminderActivity.kt
@@ -1,8 +1,10 @@
 package com.example.thyroidhelper
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 
 class ReminderActivity : AppCompatActivity() {
 
@@ -10,6 +12,12 @@ class ReminderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminder)
         setTitle(R.string.reminder_title)
+
+        if (Build.VERSION.SDK_INT < 27) {
+            this.window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED)
+        } else {
+            setShowWhenLocked(true)
+        }
     }
 
     @Suppress("UNUSED_PARAMETER")
