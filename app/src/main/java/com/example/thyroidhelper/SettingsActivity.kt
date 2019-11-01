831dc90ec5a0fb4b5fed9b25fbd3b79092b24d28
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-11-01 19:59:58 -0300

    Use time picker dialog in preferences

--- app/src/main/java/com/example/thyroidhelper/SettingsActivity.kt
+++ app/src/main/java/com/example/thyroidhelper/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.thyroidhelper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -22,8 +23,23 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+        // The preference library has a boneheaded and inextensible design so we need to override
+        // this function and copy paste some code from the original implementations.
+        override fun onDisplayPreferenceDialog(preference: Preference?) {
+            if (preference is TimePreference) {
+                val fragment = TimePreferenceDialogFragmentCompat()
+                val bundle = Bundle(1); bundle.putString("key", preference.getKey())
+                fragment.setArguments(bundle)
+                fragment.setTargetFragment(this, 0)
+                fragment.show(fragmentManager!!, null)
+            } else {
+                super.onDisplayPreferenceDialog(preference)
+            }
         }
     }
 }
