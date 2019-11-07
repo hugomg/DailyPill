f1f983ea93e0e153cdc5948967c7947befe85750
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-11-07 01:15:42 -0300

    Reset alarm after pressing the button

--- app/src/main/java/br/com/gualandi/dailypill/Notifications.kt
+++ app/src/main/java/br/com/gualandi/dailypill/Notifications.kt
@@ -95,25 +95,19 @@ object Notifications: SharedPreferencesListener {
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        Thread(Runnable {
-            if (key.equals(DataModel.DRUG_TAKEN_TIMESTAMP)
-                || key.equals(DataModel.REMINDER_ENABLED)
-                || key.equals(DataModel.REMINDER_TIME)
-            ) {
+        if (key.equals(DataModel.DRUG_TAKEN_TIMESTAMP)
+            || key.equals(DataModel.REMINDER_ENABLED)
+            || key.equals(DataModel.REMINDER_TIME)
+        ) {
+            Thread(Runnable {
                 possiblyCancelTheNotification()
-            }
-
-            if (key.equals(DataModel.REMINDER_ENABLED)
-                || key.equals(DataModel.REMINDER_TIME)
-            ) {
-
                 if (DataModel.reminderIsEnabled()) {
                     addAlarm(Calendar.getInstance(), false)
                 } else {
                     removeAlarm()
                 }
-            }
-        }).start()
+            }).run()
+        }
     }
 
     fun possiblyCancelTheNotification() {
