6cb0ac3c5a423e9f969e31572c21020f2a910fb4
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2019-11-12 00:22:19 -0300

    More consistent toolbar theming logic

--- app/src/main/java/br/com/gualandi/dailypill/SettingsActivity.kt
+++ app/src/main/java/br/com/gualandi/dailypill/SettingsActivity.kt
@@ -31,11 +31,12 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings, SettingsFragment())
             .commit()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onSupportNavigateUp() : Boolean{
